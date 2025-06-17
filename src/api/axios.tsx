402405e6e8bd0d56c998e3c363a248202961bfd9
Ochaworldwide <lazarusgabriel000@gmail.com>
2025-06-17 15:34:08 +0100

    successfully merged

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -47,7 +47,7 @@
 import axios from "axios";
 import { showSuccessToast, showErrorToast } from "@/utils/toast"; 
 
-export const BASE_URL = import.meta.env.VITE_API_URL.replace("/api", "");
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
